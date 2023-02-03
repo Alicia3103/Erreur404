abab9cc63a477796e033e32d35bcfdf75b9d6c55
Alicia3103 <charlot.alicia3103@gmail.com>
2023-02-03 17:45:52 +0100

    structure fonctionnelle modification des images des box a prévoire ainsi que la fenetre win + animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,80 +1,94 @@
 // mix les tetes de chat
-const cat1="./Public/img/tete_chat/cat_1.png",
-cat2="./Public/img/tete_chat/cat_2.png",
-cat3="./Public/img/tete_chat/cat_3.png",
-cat4="./Public/img/tete_chat/cat_4.png",
-cat5="./Public/img/tete_chat/cat_5.png";
+const cat1 = './Public/img/tete_chat/cat_1.png',
+	cat2 = './Public/img/tete_chat/cat_2.png',
+	cat3 = './Public/img/tete_chat/cat_3.png',
+	cat4 = './Public/img/tete_chat/cat_4.png',
+	cat5 = './Public/img/tete_chat/cat_5.png',
+	cat6 = './Public/img/tete_chat/cat_6.png',
+	cat7 = './Public/img/tete_chat/cat_7.png',
+	cat8 = './Public/img/tete_chat/cat_8.png',
+	cat9 = './Public/img/tete_chat/cat_9.png',
+	cat10 = './Public/img/tete_chat/cat_10.png',
+	cat11 = './Public/img/tete_chat/cat_11.png',
+	cat12 = './Public/img/tete_chat/cat_12.png',
+	cat13 = './Public/img/tete_chat/cat_13.png'
 
-let array =[cat1,cat2,cat3,cat4,cat5] 
+let array = [
+	cat1,
+	cat2,
+	cat3,
+	cat4,
+	cat5,
+	cat6,
+	cat7,
+	cat8,
+	cat9,
+	cat10
+]
 // mélange des têtes de chats
 function shuffle(array) {
-    const newArray = array;
-    const length = newArray.length;
-  
-    for (let start = 0; start < length; start++) {
-      const randomPosition = Math.floor((newArray.length - start) * Math.random());
-      const randomItem = newArray.splice(randomPosition, 1);
-  
-      newArray.push(...randomItem);
-    }
+	const newArray = array
+	const length = newArray.length
 
-    return newArray
+	for (let start = 0; start < length; start++) {
+		const randomPosition = Math.floor((newArray.length - start) * Math.random())
+		const randomItem = newArray.splice(randomPosition, 1)
+
+		newArray.push(...randomItem)
+	}
+
+	return newArray
 }
 
- // Génération d'un nombre random
-function randomNumber(min, max) {                     
-
-    return Math.floor(Math.random() * (max - min) + min);                  
-
+// Génération d'un nombre random
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min) + min)
 }
 
 //Attribution aléatoire des têtes de chat
- function attributeCat(array){
-    const shuffledArray=shuffle(array);
-    let catContainers=document.querySelectorAll('.cat');
-    
-        console.log(shuffledArray)
-    for(let i=0;i<catContainers.length;i++){
-        catContainers[i].src=shuffledArray[i];
-       
-    }
-    const randomCat=randomNumber(0,catContainers.length)
+function attributeCat(array) {
+	const shuffledArray = shuffle(array)
+	let catContainers = document.querySelectorAll('.cat')
 
+	for (let i = 0; i < catContainers.length; i++) {
+		catContainers[i].src = shuffledArray[i]
+	}
+	const randomCat = randomNumber(0, catContainers.length)
 
-    searchedCat(shuffledArray,randomCat,catContainers)
-    
+	const startGame = document.getElementById('start_game')
+	startGame.addEventListener('click', (e) => {
+		searchedCat(shuffledArray, randomCat, catContainers)
+	})
 }
-
 
 //Affichage de la tête de chat à chercher
-function searchedCat(shuffledArray,randomCat,catContainers){
-    const searchCat = shuffledArray[randomCat];
+async function searchedCat(shuffledArray, randomCat, catContainers) {
+	const searchCat = await shuffledArray[randomCat]
 
-    const findCat = document.getElementById('find_cat');
-   findCat.src= searchCat;
-  
-        
-    findTheCat(catContainers,searchCat);
+	const findCat = document.createElement('IMG')
+	findCat.src = searchCat
+	const oldP = document.getElementById('reponse_1-p')
+	document.getElementById('reponse_1-parent').replaceChild(findCat, oldP)
+
+	findTheCat(catContainers, searchCat)
 }
 //Trouver le bon chat
-function findTheCat(catContainers,searchCat){
+function findTheCat(catContainers, searchCat) {
+	catContainers.forEach((catContainer) => {
+		catContainer.addEventListener('click', (e) => {
+			const catSrc = e.currentTarget.getAttribute('src')
+			const gameResult = document.getElementById('game_result')
 
-    catContainers.forEach((catContainer) => {
-
-        catContainer.addEventListener('click', (e) => {
-            const catSrc= e.currentTarget.getAttribute('src')
-            const gameResult = document.getElementById('game_result');
-            
-                if(catSrc===searchCat){
-                        console.log('yeaah')
-                    gameResult.style.color='green'
-                    gameResult.innerText='Vous avez gagné';
-                }else{
-                    gameResult.style.color='red'
-                    gameResult.innerText='Cherchez encore';
-                }
-           
-        })
-    });
+			if (catSrc === searchCat) {
+				console.log('yeaah')
+				gameResult.style.color = 'green'
+				gameResult.innerText = 'Vous avez gagné'
+			} 
+		})
+	})
 }
+
 attributeCat(array);
+
+
+
