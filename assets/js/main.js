90ca2727537c7cb0bce6a2122c0e3d39a48c71b0
Alicia3103 <charlot.alicia3103@gmail.com>
2023-02-04 12:19:36 +0100

    page finalisée animations ok structure et jeu ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// mix les tetes de chat
+// attribution des tetes de chat
 const cat1 = './Public/img/tete_chat/cat_1.png',
 	cat2 = './Public/img/tete_chat/cat_2.png',
 	cat3 = './Public/img/tete_chat/cat_3.png',
@@ -8,23 +8,9 @@ const cat1 = './Public/img/tete_chat/cat_1.png',
 	cat7 = './Public/img/tete_chat/cat_7.png',
 	cat8 = './Public/img/tete_chat/cat_8.png',
 	cat9 = './Public/img/tete_chat/cat_9.png',
-	cat10 = './Public/img/tete_chat/cat_10.png',
-	cat11 = './Public/img/tete_chat/cat_11.png',
-	cat12 = './Public/img/tete_chat/cat_12.png',
-	cat13 = './Public/img/tete_chat/cat_13.png'
+	cat10 = './Public/img/tete_chat/cat_10.png'
 
-let array = [
-	cat1,
-	cat2,
-	cat3,
-	cat4,
-	cat5,
-	cat6,
-	cat7,
-	cat8,
-	cat9,
-	cat10
-]
+let array = [cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10]
 // mélange des têtes de chats
 function shuffle(array) {
 	const newArray = array
@@ -33,10 +19,8 @@ function shuffle(array) {
 	for (let start = 0; start < length; start++) {
 		const randomPosition = Math.floor((newArray.length - start) * Math.random())
 		const randomItem = newArray.splice(randomPosition, 1)
-
 		newArray.push(...randomItem)
 	}
-
 	return newArray
 }
 
@@ -46,6 +30,7 @@ function randomNumber(min, max) {
 }
 
 //Attribution aléatoire des têtes de chat
+
 function attributeCat(array) {
 	const shuffledArray = shuffle(array)
 	let catContainers = document.querySelectorAll('.cat')
@@ -53,93 +38,110 @@ function attributeCat(array) {
 	for (let i = 0; i < catContainers.length; i++) {
 		catContainers[i].src = shuffledArray[i]
 	}
+	//calcul d'un nombre aléatoire pour déterminer la position (dans le tableau)de la tête de chat a trouver
 	const randomCat = randomNumber(0, catContainers.length)
 
-	const startGame = document.getElementById('start_game');
-	const oldCat=document.getElementById('oldCat');
-console.log(oldCat)
-	
+	const startGame = document.getElementById('start_game')
+	const oldCat = document.getElementById('oldCat')
+
+	// si on clique pour initier le jeu
 	startGame.addEventListener('click', (e) => {
 		searchedCat(shuffledArray, randomCat, catContainers)
 	})
-	if (oldCat){
-		
-		newSearchedCat(shuffledArray, randomCat, catContainers)
+
+	//si le jeu est relancé
+	if (oldCat) {
+		searchedCat(shuffledArray, randomCat, catContainers)
 	}
 }
 
 //Affichage de la tête de chat à chercher
-async function searchedCat(shuffledArray, randomCat, catContainers) {
-	const searchCat = await shuffledArray[randomCat]
-
+function searchedCat(shuffledArray, randomCat, catContainers) {
+	const searchCat = shuffledArray[randomCat]
+	//création de l'élément et attribution de l'image a chercher
 	const findCat = document.createElement('IMG')
-	findCat.setAttribute('id','newCat')
+	findCat.setAttribute('id', 'newCat')
 	findCat.src = searchCat
-	const oldP = document.getElementById('reponse_1-p')
-	if(oldP){
-	document.getElementById('reponse_1-parent').replaceChild(findCat, oldP)	
-	}
-	
-	findTheCat(catContainers, searchCat)
-}
 
-//Affichage de la tête de chat à chercher
-async function newSearchedCat(shuffledArray, randomCat, catContainers) {
-	const searchCat = await shuffledArray[randomCat]
+	const rep = document.getElementById('reponse_1-p')
 
-	const findCat = document.createElement('IMG')
-	findCat.setAttribute('id','newCat')
-	findCat.src = searchCat
 	const oldP = document.getElementById('oldCat')
-	document.getElementById('reponse_1-parent').replaceChild(findCat, oldP)
+	//si le jeu est joué pour la première foi
+	if (rep) {
+		document.getElementById('reponse_1-parent').replaceChild(findCat, rep)
+	}
+	//si le jeu est rejoué
+	else if (oldP) {
+		document.getElementById('reponse_1-parent').replaceChild(findCat, oldP)
+	}
 
 	findTheCat(catContainers, searchCat)
 }
+
 //Trouver le bon chat
 function findTheCat(catContainers, searchCat) {
+	//ecoute pour toutes les boites à chat
 	catContainers.forEach((catContainer) => {
 		catContainer.addEventListener('click', (e) => {
 			const catSrc = e.currentTarget.getAttribute('src')
 			const gameResult = document.getElementById('game_result')
-
+			//si l'image est la même que celle à trouver
 			if (catSrc === searchCat) {
-				console.log('yeaah')
-				gameResult.style.color = 'green'
+				//affichage de la victoire et possibilité de rejouer
+				
 				gameResult.innerText = 'Vous avez gagné'
-				gameResult.innerHTML=`
+				gameResult.innerHTML = `
 				<div class="result__container"> <div class="result__content">
 				<p>Félicitations Vous avez gagné</p>
-				<p>Voulez-vous <span id="new__game">rejouer</span></p>
+				<p>Voulez-vous <span id="new__game">rejouer?</span></p>
+				<div id="close">X</div>
 				</div></div>`
-				gameResult.style.zIndex = "10"
-				gameResult.style.transform='translateY(-420px)';
-				gameResult.style.transition=" all 1s" 
-				newGame(array,gameResult)
-			} 
+				gameResult.style.zIndex = '10'
+				gameResult.style.transform = 'translateY(-420px)'
+				gameResult.style.transition = ' all 0.5s'
+
+				newGame(array, gameResult)
+			}
 		})
 	})
 }
 
-attributeCat(array);
+//relancer le jeu
+function newGame(array, gameResult) {
+	const newGameSpan = document.getElementById('new__game')
+	const endGame=document.getElementById('close')
+	const oldCat = document.getElementById('newCat')
 
-
-
- function newGame(array, gameResult){
-	const newGameSpan= document.getElementById('new__game')
-	
-	newGameSpan.addEventListener('click',(e)=>{
-		
-		gameResult.style.zIndex = "-10"
-		console.log('rejouer')
-		oldCat=document.getElementById('newCat')
-		oldCat.setAttribute('id','oldCat')
-		attributeCat(array);
-
-		//penser a clean gameResult
-		
+	//ne pas relancer le jeu et revenir à la configuration du début
+	endGame.addEventListener('click', (e) => {
+		//clean gameResult
+		gameResult.style.zIndex = '-10'
 		gameResult.innerText = ''
-				gameResult.innerHTML=``
-			
+		gameResult.innerHTML = ``
+
+		const rejouer= document.createElement('p')
+		rejouer.innerText='Oui je veux jouer' 
+		rejouer.setAttribute('id', 'reponse_1-p')
+		document.getElementById('reponse_1-parent').replaceChild(rejouer, oldCat)
+		
+		attributeCat(array)
 
 	})
+
+	//rejouer 
+	newGameSpan.addEventListener('click', (e) => {
+
+		//modification de l'id de l'ancienne photo à trouver pour pouvoir la remplacer
+		oldCat.setAttribute('id', 'oldCat')
+
+		attributeCat(array)
+
+		//clean gameResult
+		gameResult.style.zIndex = '-10'
+		gameResult.innerText = ''
+		gameResult.innerHTML = ``
+	})
 }
+
+
+attributeCat(array)
